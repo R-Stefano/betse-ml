924a0db945168be1f72351b66f90ccb649e4d48a
steve <stefano.bane@gmail.com>
2021-03-10 15:46:13 +0000

    updated bash script

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+#sudo apt-get install git
+#git clone https://R-Stefano:M4kemoney100%@github.com/R-Stefano/betse-ml.git
+
 sudo apt update
 sudo apt install python3 python3-dev python3-venv
 sudo apt install wget
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
-
-sudo apt-get install git
-git clone https://github.com/R-Stefano/betse-ml.git
-echo 'R-Stefano'
-echo 'M4kemoney100%'
 
 cd betse-ml
 
