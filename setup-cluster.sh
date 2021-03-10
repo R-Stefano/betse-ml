7a1ce1a5368a9a2b0a48ae69573480e7cd680249
steve <stefano.bane@gmail.com>
2021-03-10 16:09:01 +0000

    updated bash script

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -2,15 +2,15 @@
 #sudo apt-get install git
 #git clone https://R-Stefano:M4kemoney100%@github.com/R-Stefano/betse-ml.git
 
-sudo apt update
+sudo apt update --force-yes
 sudo apt install python3 python3-dev python3-venv
 sudo apt install wget
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 
-cd betse-ml
-
 pip install pyyaml pandas torch matplotlib betse
 
 
 echo 'export PATH="~/.local/lib/python3.7/site-packages/:$PATH"' > ~/.bashrc
+
+sudo reboot
