fffb9ed6a9065082971fd9de0c7c0e97273a2694
steve <stefano.bane@gmail.com>
2021-03-15 15:23:40 +0000

    changes

--- generate/configs.py
+++ generate/configs.py
@@ -5,7 +5,7 @@ bucketName = "betse-ml"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './generate/generate-worker.json'
 
 simulation_timestep = 1.0e-3
-simulation_duration_s = 30.01 # 60.01
+simulation_duration_s = 60.01 # 60.01
 simulation_sampling_rate = 5 #simulation_duration_s - 0.01
 
 SIM_RUNS = 500
